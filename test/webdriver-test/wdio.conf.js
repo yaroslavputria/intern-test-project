bda5fc31163b4f79149dca79053c6b81cf88878b
yaroslav.putria <yaroslav.putria@avid.com>
2016-09-08 14:29:23 +0300

    ITS ALIVE!

--- test/webdriver-test/wdio.conf.js
+++ test/webdriver-test/wdio.conf.js
@@ -1,28 +1,11 @@
 exports.config = {
 
-    /**
-     * server configurations
-     */
     host: '127.0.0.1',
     port: 4444,
 
-    /**
-     * specify test files
-     */
-    specs: [
-        'test/webdriver-test/test.js'
-    ],
-    // exclude: [
-    //     'test/spec/multibrowser/**',
-    //     'test/spec/mobile/**'
-    // ],
+    specs: ['test/webdriver-test/test.js'],
 
-    /**
-     * capabilities
-     */
-    capabilities: [{
-        browserName: 'chrome'
-    }],
+    capabilities: [{ browserName: 'chrome' }],
 
     desiredCapabilities: [{
         browserName: 'chrome',
@@ -37,19 +20,10 @@ exports.config = {
     user: "yaroslavputria",
     key: "e3f7b71c-1eb3-47b1-bc30-60dad6b75093",
     sauceConnect: true,
-    //services: ['phantomjs'],
-    /**
-     * test configurations
-     */
     logLevel: 'silent',
     coloredLogs: true,
     waitforTimeout: 30000,
-    //waitforInterval: 30000,
     framework: 'mocha',
-    // mochaOpts: {
-    //     timeout: 60000
-    // },
-
     reporters: ['spec'],
     reporterOptions: {
         outputDir: './'
@@ -59,9 +33,6 @@ exports.config = {
         ui: 'bdd'
     },
 
-    /**
-     * hooks
-     */
     onPrepare: function() {
         console.log('let\'s go');
     },
