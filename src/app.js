6db248e9e2f0a3e3177f77b43bc1aef4e5af9c22
yaroslav.putria <yaroslav.putria@avid.com>
2016-09-14 16:23:46 +0300

    backed to require.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
 'use strict';
 
 var ROOT_EL = document.body;
+require(['./src/model/model.js', './src/view/form.js', './src/view/results.js'], function (Model, Form, View) {
+  var model = new Model();
 
-var Model = require('./model/model.js');
-var model = new Model();
+  var form = new Form({
+    arrOfLangs: ['', 'JSON', 'JavaScript', 'Text', 'Markdown'],
+    arrOfTypes: ['', 'application/json', 'application/javascript', 'text/plain'],
+    rootEl: ROOT_EL,
+  });
 
-var Form = require('./view/form.js');
-var form = new Form({
-  arrOfLangs: ['', 'JSON', 'JavaScript', 'Text', 'Markdown'],
-  arrOfTypes: ['', 'application/json', 'application/javascript', 'text/plain'],
-  rootEl: ROOT_EL,
-});
+  var view = new View();
 
-var View = require('./view/results.js');
-var view = new View();
-
-form.addProcessingListener(function (filterCriterias) {
-  model.getFileNamesPromise(filterCriterias).then(function (files) {
-    view.createResultList(files, ROOT_EL);
+  form.addProcessingListener(function (filterCriterias) {
+    model.getFileNamesPromise(filterCriterias).then(function (files) {
+      view.createResultList(files, ROOT_EL);
+    });
   });
 });
